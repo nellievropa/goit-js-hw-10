0488fcd4f8e0d223aa8d8cb32857a46948102da7
nellievropa <nellievropa23@gmail.com>
2023-12-12 16:32:50 +0200

    getзапит

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// const API_KEY = 'live_z6eGqgD1UKta40zwjx9gIJFQe07jIJtgN9xqenG2iTX9aiF4AElDeCIQ3o4r162B';
+// const BASE_URL = 'https://api.thecatapi.com/v1';
+// const options = {
+//     headers: {
+//         Authorization: API_KEY,
+//     },
+//     };
+
+// const getBreed = getBreed => {
+//     const url = `${BASE_URL}/images/search?breed_ids=${breed.id}`;
+//     fetch(url, options).then(res => res.json());
+//     return getBreed;
+// };
+
+// getBreed(beng).then(breed => console.log(breed));
+
+const headers = new Headers({
+  "Content-Type": "application/json",
+  "x-api-key": "DEMO-API-KEY"
+});
+
+var requestOptions = {
+  method: 'GET',
+  headers: headers,
+  redirect: 'follow'
+};
+
+fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=100", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));   
