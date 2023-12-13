603911e00931b1b1b8383e5f5851beea4bb3aca8
nellievropa <124687833+nellievropa@users.noreply.github.com>
2023-12-13 15:16:32 +0000

    Deploying to gh-pages from @ nellievropa/goit-js-hw-10@6d024fe6789fa90f911d49a3af2c7776c06435c5 🚀

--- index.73225940.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=new Headers({"Content-Type":"application/json","x-api-key":"live_z6eGqgD1UKta40zwjx9gIJFQe07jIJtgN9xqenG2iTX9aiF4AElDeCIQ3o4r162B"});fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=44",{method:"GET",headers:e,redirect:"follow"}).then((function(e){return e.text()})).then((function(e){return console.log(e)})).catch((function(e){return console.log("error",e)}))}();
-//# sourceMappingURL=index.73225940.js.map
