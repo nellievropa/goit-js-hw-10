6d024fe6789fa90f911d49a3af2c7776c06435c5
nellievropa <nellievropa23@gmail.com>
2023-12-13 17:16:10 +0200

    start

--- src/index.js
+++ src/index.js
@@ -14,18 +14,66 @@
 
 // getBreed(beng).then(breed => console.log(breed));
 
-const headers = new Headers({
-  "Content-Type": "application/json",
-  "x-api-key": "live_z6eGqgD1UKta40zwjx9gIJFQe07jIJtgN9xqenG2iTX9aiF4AElDeCIQ3o4r162B"
-});
+// const headers = new Headers({
+//   "Content-Type": "application/json",
+//   "x-api-key": "live_z6eGqgD1UKta40zwjx9gIJFQe07jIJtgN9xqenG2iTX9aiF4AElDeCIQ3o4r162B"
+// });
 
-var requestOptions = {
-  method: 'GET',
-  headers: headers,
-  redirect: 'follow'
-};
+// var requestOptions = {
+//   method: 'GET',
+//   headers: headers,
+//   redirect: 'follow'
+// };
 
-fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=44", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));   
+// fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=44", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
+
+const search = document.querySelector('.js-breed-select');
+search.addEventListener('click', onSearchBreed);
+let storedBreeds = [];
+
+function onSearchBreed(evt) {
+  evt.preventDefault();
+  console.log(evt.curenttarget.elemnts.value)
+}
+
+function fetchBreeds() {
+  const BASE_URL = 'https://api.thecatapi.com/v1';
+  const API_KEY = 'live_z6eGqgD1UKta40zwjx9gIJFQe07jIJtgN9xqenG2iTX9aiF4AElDeCIQ3o4r162B';
+
+  return fetch(`${BASE_URL}/breeds?${API_KEY}/images/search`)
+    .then(response => {
+      if (!response.ok) {
+      throw new Error(response.statusText)
+      }
+       return response.json();
+    })
+  
+ 
+}
+
+fetchBreeds()
+  .then((data) => {
+    // data = data.filter(img=> img.image?.url!=null)
+   
+  storedBreeds = data.currentTarget.id;
+   
+    for (let i = 0; i < storedBreeds.length; i++) {
+      const breed = storedBreeds[i];
+      let option = document.createElement('option');
+     
+      //skip any breeds that don't have an image
+      if (!breed.image) continue
+     
+      //use the current array index
+      option.value = i;
+      option.innerHTML = `${breed.name}`;
+      document.getElementById('breed_selector').appendChild(option);
+    }
+    console.log(storedBreeds)
+  })
+  .catch(error => console.log(error));
+
+
