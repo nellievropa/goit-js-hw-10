c9cf3b333203418ba04e6d6d20e3b7419d6d2788
nellievropa <nellievropa23@gmail.com>
2023-12-20 16:41:22 +0200

    sreate arrey!

--- src/index.js
+++ src/index.js
@@ -37,54 +37,56 @@ let storedBreeds = [];
 
 function onSearchBreed(evt) {
   evt.preventDefault();
-  const { breed } = evt.curenttarget.breed.id.value;
-  fetchBreeds(breed.id.value)
-    .then(data => console.log(data))
+  // const { breed } = evt.curenttarget.breed.id.value;
+  fetchBreeds()
+    .then(storedBreeds => (option.innerHTML = createMarkup(data.name)))
     .catch(error => console.log(error));
-  console.log(breed.id)
+   console.log(storedBreeds)
 }
 
-function fetchBreeds() {
+function fetchBreeds(id) {
   const BASE_URL = 'https://api.thecatapi.com/v1';
   const API_KEY = 'live_z6eGqgD1UKta40zwjx9gIJFQe07jIJtgN9xqenG2iTX9aiF4AElDeCIQ3o4r162B';
 
   return fetch(
-    `${BASE_URL}/breeds?${API_KEY}/images/search`
+    `${BASE_URL}/breeds?${API_KEY}&id=${id}`
   ).then(response => {
     console.log(response.json())
       if (!response.ok) {
       throw new Error(response.statusText)
       }
-       return response.json();
+    return response.json();
+    
     })
-  
  
 }
 
 function createMarkup(array) {
-  
+  return array.map(({ name  }) => `<option 
+  value="breed-name">${ name} 
+  </option>`).join('')
 }
 
-fetchBreeds()
-  .then((data) => {
-    // data = data.filter(img=> img.image?.url!=null)
+// fetchBreeds()
+//   .then((data) => {
+//     // data = data.filter(img=> img.image?.url!=null)
    
-  storedBreeds = data.currentTarget.id;
+//   storedBreeds = data.currentTarget.id;
    
-    for (let i = 0; i < storedBreeds.length; i++) {
-      const breed = storedBreeds[i];
-      let option = document.createElement('option');
+//     for (let i = 0; i < storedBreeds.length; i++) {
+//       const breed = storedBreeds[i];
+//       let option = document.createElement('option');
      
-      //skip any breeds that don't have an image
-      if (!breed.image) continue
+//       //skip any breeds that don't have an image
+//       if (!breed.image) continue
      
-      //use the current array index
-      option.value = i;
-      option.innerHTML = `${breed.name}`;
-      document.getElementById('breed_selector').appendChild(option);
-    }
-    console.log(storedBreeds)
-  })
-  .catch(error => console.log(error));
+//       //use the current array index
+//       option.value = i;
+//       option.innerHTML = `${breed.name}`;
+//       document.getElementById('breed_selector').appendChild(option);
+//     }
+//     console.log(storedBreeds)
+//   })
+//   .catch(error => console.log(error));
 
 
